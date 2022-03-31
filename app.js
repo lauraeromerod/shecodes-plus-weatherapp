aab1b7fd7828d9b0a33f97cdce52b4a18cd5bc77
elena-laura <69767636+lauraeromerod@users.noreply.github.com>
2022-04-01 00:25:31 +0200

    :lipstick: new style changes

--- app.js
+++ app.js
@@ -229,13 +229,15 @@ function getForecastWeatherData(response) {
     }).format(day);
     const forecastDescription = weather[0].description;
 
-    forecastWrapper.innerHTML += `<div class="col-12 col-sm-6 mb-3">
+    forecastWrapper.innerHTML += `<div class="col-12 col-md-4 col-sm-6 mb-3">
     <div class="c-weather__box c-weather__box--${index}">
+    <div>
     <h3 class="c-weather__day c-weather__day--active">${formattedDay}</h3>
     <p class="c-weather__temperature">
       <span class="c-weather__high-temperature">${highTemperature}°</span>
       <span class="c-weather__low-temperature">${lowTemperature}°</span>
     </p>
+    </div>
     <p class="c-weather__description">${forecastDescription}</p>
     <img src="${iconObject.src}" alt="${iconObject.alt}" class="c-weather__icon" />
   </div>
